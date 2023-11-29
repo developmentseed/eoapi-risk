1211f37243513d5a38e0ffaaa1a3e543f441ced5
Tarashish Mishra <sunu@sunu.in>
2023-11-29 10:27:21 +0530

    add maxar data ingestion example

---
+++ ingest/datasets/maxar_opendata/README.md
@@ -0,0 +1,3 @@
+Maxar Open Data: https://www.maxar.com/open-data
+
+Code adapted from: https://github.com/vincentsarago/MAXAR_opendata_to_pgstac
---
+++ ingest/datasets/maxar_opendata/src/generate_items.py
@@ -0,0 +1,48 @@
+"""Create STAC Collections and Items files."""
+
+import json
+import logging
+import pathlib
+import pystac
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def generate(output_dir="."):
+    """Generate STAC Collections and Items files for Maxar Open Data."""
+    logger.info("Connecting to static catalog...")
+    catalog = pystac.Catalog.from_file("https://maxar-opendata.s3.amazonaws.com/events/catalog.json")
+
+    collections = list(catalog.get_collections())
+    logger.info(f"Found {len(collections)} collections")
+    logger.info(collections)
+
+    logger.info("Creating collections.json file...")
+    with open(pathlib.Path(output_dir)/"collections.json", "w") as f:
+        for collection in collections:
+            c = collection.to_dict()
+            c["links"] = []
+            c["id"] = "MAXAR_" + c["id"].replace("-","_")
+            c["description"] = "Maxar OpenData | " + c["description"]
+            f.write(json.dumps(c) + "\n")
+
+
+    logger.info("Creating items .json files...")
+    for collection in collections:
+        collection_id = "MAXAR_" + collection.id.replace("-","_")
+        logger.info(f"Processing items for {collection_id}")
+        with open(pathlib.Path(output_dir)/f"{collection_id}_items.json", "w") as f:
+
+            # Each Collection has collections
+            for c in collection.get_collections():
+
+                # Loop through each items
+                # edit items and save into a top level collection JSON file
+                for item in c.get_all_items():
+                    item_dict = item.make_asset_hrefs_absolute().to_dict()
+                    item_dict["links"] = []
+                    item_dict["collection"] = collection_id
+                    item_dict["id"] = item.id.replace("/", "_")
+                    item_str = json.dumps(item_dict)
+                    f.write(item_str + "\n")
