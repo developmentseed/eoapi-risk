3c5ee348e8a35a372a820fe5cca0a6f9ab0eb413
Rub21 <ruben@developmentseed.org>
2024-01-11 11:45:45 -0500

    Fix DATABASE_URL

--- ingest/entrypoint.sh
+++ ingest/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
+set -x
+export DATABASE_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASS}@${PGHOST}:${PGPORT}/${POSTGRES_DBNAME}"
 dataOutput=/data
-
 mkdir -p $dataOutput
-
 python entrypoint.py population
 python entrypoint.py admin_boundaries
 python entrypoint.py buildings
