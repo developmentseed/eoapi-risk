537983a0e8b7a8ea3ab230cbbd7a9320e0889ffa
Rub21 <ruben@developmentseed.org>
2024-01-11 09:52:19 -0500

    fix docker image

--- ingest/entrypoint.sh
+++ ingest/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
+set -x
+export DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASS}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DBNAME}"
 dataOutput=/data
-
 mkdir -p $dataOutput
-
 python entrypoint.py population
 python entrypoint.py admin_boundaries
 python entrypoint.py buildings
