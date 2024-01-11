a3ffee7b0ac41290feef301c784b34beffeb9bd8
Rub21 <ruben@developmentseed.org>
2024-01-11 13:02:03 -0500

    Update entrypoints

--- ingest/entrypoint.sh
+++ ingest/entrypoint.sh
@@ -5,5 +5,7 @@ dataOutput=/data
 mkdir -p $dataOutput
 python entrypoint.py population
 python entrypoint.py admin_boundaries
-python entrypoint.py health_facilities
 python entrypoint.py buildings
+python entrypoint.py health_facilities
+python entrypoint.py maxar_opendata
+python entrypoint.py shakemap_peak
