4bb5160d2919c4f3e5c647df14c41444d179e479
Rub21 <ruben@developmentseed.org>
2024-01-12 12:44:57 -0500

    Set collection and items for population

--- ingest/datasets/population/process.py
+++ ingest/datasets/population/process.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 PAGE_LINK = "https://data.humdata.org/dataset/kontur-population-afghanistan"
 REGEX_URL = r'<a\s+(?:[^>]*?\s+)?href="(http[s]?://[^"]*)"'
 STAC_VERSION = "1.0.0"
-COLLECTION = "population"
+COLLECTION = "population_hexbins"
 ITEM = f"{COLLECTION}_afghanistan"
 TITLE = "Afghanistan, Population Density for 400m H3 Hexagons"
 DESCRIPTION = "Built from Kontur Population, Global Population Density for 400m H3 Hexagons Vector H3 hexagons with population counts at 400m resolution"
@@ -55,33 +55,43 @@ def download_data(link, file_tmp_path):
 
 
 def run(path_local):
+    #################
+    # Load collection into the DB
+    #################
+    # logger.info("\n\nLoad collection into the DB..")
+    # stac_collection_path = f"datasets/population/collection.json"
+    # logger.info("Importing colletion to pgstac...")
+    # output_json = run_cli(
+    #     ["pypgstac", "load", "collections"],
+    #     stac_collection_path,
+    #     {"--method": "insert_ignore", "--dsn": environ["DATABASE_URL"]},
+    # )
+    # #################
+    # Read and Save geo data in the DB
+    # #################
+    # logger.info("\n\nRead and Save geo data in the DB...")
     link = get_link()
-    makedirs(path_local, exist_ok=True)
-    file_name = link.split("/")[-1]
-    file_gpkg = download_data(link, f"{path_local}/{file_name}")
-    gdf = gpd.read_file(file_gpkg)
-    gdf = gdf.to_crs(4326)
-    gdf["id"] = gdf.index
-
+    # makedirs(path_local, exist_ok=True)
+    # file_name = link.split("/")[-1]
+    # file_gpkg = download_data(link, f"{path_local}/{file_name}")
+    # gdf = gpd.read_file(file_gpkg)
+    # gdf = gdf.to_crs(4326)
+    # gdf["id"] = gdf.index
     file_path = f"{path_local}/{ITEM}.geojson"
-    gdf.to_file(file_path, driver="GeoJSON")
-    # #################
-    # save in database
-    # #################
-    print("Saving dataset in DB..")
-    save_postgis(
-        gdf=gdf,
-        table_name=ITEM,
-        if_exists="replace",
-        index=True,
-        schema="pgstac",
-        table_id="id",
-    )
+    # gdf.to_file(file_path, driver="GeoJSON")
+    # save_postgis(
+    #     gdf=gdf,
+    #     table_name=ITEM,
+    #     if_exists="replace",
+    #     index=False,
+    #     schema="public",
+    #     table_id="id",
+    # )
 
     # #################
-    # save item stac
+    # Save Item stac in the DB
     # #################
-    logger.info("Running fio stac for dataset...")
+    logger.info("\n\nSave Item stac in the DB...")
     args = {
         "--id": ITEM,
         "--datetime": DATETIME,
@@ -94,22 +104,18 @@ def run(path_local):
     output_json["output"]["description"] = DESCRIPTION
     output_json["output"]["license"] = LICENSE
     output_json["output"]["table"] = ITEM
-    output_json["output"]["links"] = {
-        "href": link,
-        "rel": link,
-        "title": TITLE,
-    }
-    stac_item_path = f"{path_local}/{ITEM}_stac_item_.json"
-
+    output_json["output"]["links"] = [
+        {
+            "href": link,
+            "rel": link,
+            "title": TITLE,
+        }
+    ]
+    stac_item_path = f"{file_path}_.json"
     with open(stac_item_path, "w") as file:
         file.write(json.dumps(output_json["output"]))
-
-    #################
-    # Run: pypgstac load collections
-    #################
-    logger.info("Importing item/colletion to pgstac...")
     output_json = run_cli(
-        ["pypgstac", "load", "collections"],
+        ["pypgstac", "load", "items"],
         stac_item_path,
         {"--method": "insert_ignore", "--dsn": environ["DATABASE_URL"]},
     )
